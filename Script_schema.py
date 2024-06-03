637fa577a8e9fcdef3434afdb364f83d1058a334
ricciric <riki.marasca@gmail.com>
2024-06-03 12:10:07 +0200

    Aggiunte ripetizioni attributo nei file txt e sorted

--- Script_schema.py
+++ Script_schema.py
@@ -1,32 +1,40 @@
 import os
 import json
+from collections import defaultdict
 
-def extract_keys(folder_path):
-    keys = set()
-    
+def extract_keys_from_json_files(folder_path):
+    """
+    Extract keys and their counts from all JSON files in the given folder.
+    """
+    key_counts = defaultdict(int)
     for root, _, files in os.walk(folder_path):
         for file in files:
-            if file.endswith(".json"):
+            if file.endswith('.json'):
                 file_path = os.path.join(root, file)
-                
                 with open(file_path, 'r') as json_file:
-                    data = json.load(json_file)
-                    keys.update(data.keys())
-    return keys
+                    try:
+                        data = json.load(json_file)
+                        for key in data.keys():
+                            key_counts[key] += 1
+                    except json.JSONDecodeError:
+                        print(f"Error decoding JSON from file: {file_path}")
+    return key_counts
 
-def write_keys_to_file(folder_path, keys):
-    folder_name = os.path.basename(folder_path)
-    output_file_path = os.path.join(folder_path, f"{folder_name}.txt")
+def write_keys_to_file(folder_path, key_counts):
     
+    folder_name = os.path.basename(folder_path)
+    output_file_path = os.path.join(os.getcwd(), "Attributes\\" + f"{folder_name}.txt")
+    sorted_key_counts = sorted(key_counts.items(), key=lambda item: item[1], reverse=True)
     with open(output_file_path, 'w') as output_file:
-        for key in sorted(keys):
-            output_file.write(f"{key}\n")
+        for key, count in sorted_key_counts:
+            output_file.write(f"{key}: {count}\n")
+            
             
 def process_folders(base_path):
     for root, dirs, _ in os.walk(base_path):
         for dir in dirs:
             folder_path = os.path.join(root, dir)
-            keys = extract_keys(folder_path)
+            keys = extract_keys_from_json_files(folder_path)
             write_keys_to_file(folder_path, keys)
             
             
