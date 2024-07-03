60811486e77748adde296cfc759ea8a0ff132d63
Umberto28 <umberto.gen28@gmail.com>
2024-07-03 11:57:06 +0200

    completed firla implementation

--- firla/dataset_creator_firla.py
+++ firla/dataset_creator_firla.py
@@ -6,25 +6,24 @@ CUR_PATH = os.getcwd()
 MEDIATED_SCHEMA = "/mediated_schema/mediated_schema.json"
 DATASET_PATH = "monitor_specs"
 
-# Read all the json files and convert them
-def read_json_files_and_create(dataset_path):
+# Function to replace Unicode escape sequences
+def decode_unicode_escapes(data):
+    # Regular expression to find Unicode escape sequences
+    unicode_escape_pattern = re.compile(r'\\u([0-9a-fA-F]{4})')
 
-    with open(CUR_PATH + MEDIATED_SCHEMA, 'r') as mediated_schema:
-        mediated_schema = json.load(mediated_schema)
-    
-    for root, _, files in os.walk(dataset_path):
-        for file in files:
-            
-            if file.endswith(".json"):
-                file_path = os.path.join(root, file)
-                
-                with open(file_path, 'r', encoding='utf-8') as json_file:
-                    data = json.load(json_file)
-                    decoded_data = decode_unicode_escapes(data)
-                    new_data = create_schema_mapping_from_data(mediated_schema, decoded_data)
-        
-                with open('firla\\monitor_specs_mediated\\' + os.path.basename(root) + '\\' + file, 'w', encoding='utf-8') as output_file:
-                    json.dump(new_data, output_file, indent=4, ensure_ascii=False)
+    # Function to replace the matched escape sequence with the corresponding character
+    def replace_match(match):
+        return chr(int(match.group(1), 16))
+
+    # Recursively replace Unicode escapes in strings, dictionaries, and lists
+    if isinstance(data, str):
+        return unicode_escape_pattern.sub(replace_match, data)
+    elif isinstance(data, dict):
+        return {key: decode_unicode_escapes(value) for key, value in data.items()}
+    elif isinstance(data, list):
+         return decode_unicode_escapes(data[0]) if data else None
+    else:
+        return data
 
 def create_schema_mapping_from_data(mediated_schema, data):
 
@@ -47,39 +46,71 @@ def create_schema_mapping_from_data(mediated_schema, data):
             
     return converted_dict
 
-# Function to replace Unicode escape sequences
-def decode_unicode_escapes(data):
-    # Regular expression to find Unicode escape sequences
-    unicode_escape_pattern = re.compile(r'\\u([0-9a-fA-F]{4})')
+# Read all the json files and convert them
+def create_mapped_dataset(dataset_path):
 
-    # Function to replace the matched escape sequence with the corresponding character
-    def replace_match(match):
-        return chr(int(match.group(1), 16))
+    with open(CUR_PATH + MEDIATED_SCHEMA, 'r') as mediated_schema:
+        mediated_schema = json.load(mediated_schema)
+    
+    for root, _, files in os.walk(dataset_path):
+        for file in files:
+            
+            if file.endswith(".json"):
+                file_path = os.path.join(root, file)
+                
+                with open(file_path, 'r', encoding='utf-8') as json_file:
+                    data = json.load(json_file)
+                    decoded_data = decode_unicode_escapes(data)
+                    new_data = create_schema_mapping_from_data(mediated_schema, decoded_data)
+        
+                with open('firla\\monitor_specs_mediated\\' + os.path.basename(root) + '\\' + file, 'w', encoding='utf-8') as output_file:
+                    json.dump(new_data, output_file, indent=4, ensure_ascii=False)
 
-    # Recursively replace Unicode escapes in strings, dictionaries, and lists
-    if isinstance(data, str):
-        return unicode_escape_pattern.sub(replace_match, data)
-    elif isinstance(data, dict):
-        return {key: decode_unicode_escapes(value) for key, value in data.items()}
-    elif isinstance(data, list):
-         return decode_unicode_escapes(data[0]) if data else None
-    else:
-        return data
+def create_blocks(record, attribute, l):
+    blocks = []
+    
+    field_value = record[attribute].lower()
+    blocks = [field_value[i:i+l] for i in range(len(field_value) - l + 1)]
 
-def value_exist_in_json(data, value):
-    for key in data.keys():
-        for val in key:
-            if value in val:
-                return True
-    return False
+    return blocks
+
+def create_mapped_schema(dataset_path):
+
+    with open(CUR_PATH + MEDIATED_SCHEMA, 'r') as mediated_schema:
+        mediated_schema = json.load(mediated_schema)
+    
+    schema_mapping = []
+
+    for root, _, files in os.walk(dataset_path):
+        for file in files:
+            
+            if file.endswith(".json"):
+                file_path = os.path.join(root, file)
+                
+                with open(file_path, 'r', encoding='utf-8') as json_file:
+                    data = json.load(json_file)
+                    decoded_data = decode_unicode_escapes(data)
+                    
+                    new_data = create_schema_mapping_from_data(mediated_schema, decoded_data)
+                    new_data['record_ID'] = os.path.basename(root) + '\\' + file
+                    new_data['record_blocks'] = create_blocks(new_data, 'name', 10)
+
+                    schema_mapping.append(new_data)
+    
+    # avg_len = 0
+    # for record in schema_mapping:
+    #     avg_len += len(record['name'])
+
+    # print('Dataset lenght = ', len(schema_mapping))
+    # print('Avarage Name Lenght = ', avg_len/len(schema_mapping))
+
+    with open('firla\\schema_mapping_firla.json', 'w', encoding='utf-8') as output_file:
+        json.dump(schema_mapping, output_file, indent=4, ensure_ascii=False)
 
 def main():
-    # Read all json files
-    schema_mapping = read_json_files_and_create(DATASET_PATH)
-    
-    # output_path = os.path.join(CUR_PATH, "schema_mapping.json")
-    # with open(output_path, 'w', encoding='utf-8') as output_file:
-    #     json.dump(schema_mapping, output_file, indent=4, ensure_ascii=False)
+    # create_mapped_dataset(DATASET_PATH)
+    create_mapped_schema(DATASET_PATH)
+
 
 if __name__ == "__main__":
     main()
--- firla/record_linkage_firla.py
+++ firla/record_linkage_firla.py
@@ -1,43 +1,33 @@
 import os
 import json
+import time
+import pstats
+import cProfile
 import numpy as np
 import Levenshtein as lev
 from dataset_creator_firla import decode_unicode_escapes
 
+THETA = 6
+BLOCK_VALUE = 10
 CUR_PATH = os.getcwd()
 DATASET_PATH = "firla\\monitor_specs_mediated"
-THETA = 4
 
 def concatenate_attributes(record):
-    attr_string = ''
-    attributes = record.values()
-
-    for attr in attributes:
-        attr_string += attr.lower()
-        attr_string += '#'
-
-    res = attr_string[:-1]
-    
-    return res
+    attr_string = '#'.join(attr.lower() for k, attr in record.items() if k != 'record_blocks')
+    return attr_string
 
 def deduplication(records):
     global offset
-    dup = False
     idx = 0
 
     for record in records.values():
-        for k, v in clusters.items():
-            if record in v:
-                # clusters[k].append(record)
-                dup = True
-                break
-        if not dup:
+        if not any(record in cluster for cluster in clusters.values()):
             clusters['C'+ str(idx + offset)] = [record]
             idx += 1
     
     offset = idx
 
-def blocking():
+def blocking(dataset):
     global clusters_blocks
     global clusters
 
@@ -46,27 +36,22 @@ def blocking():
         new_clusters = {k: v for k, v in clusters.items() if k not in clusters_blocks}
         
         for key, values in new_clusters.items():
-            if key not in clusters_blocks.keys():
-                clusters_blocks[key] = []
+            clusters_blocks.setdefault(key, set())
             
             for v in values:
-                b = v[:5]
-                if b not in clusters_blocks[key]:
-                    clusters_blocks[key].append(b)
+                record_blocks = list(filter(lambda record: record['record_ID'] == v[v.rfind('#') + 1:], dataset))
+                clusters_blocks[key].update(record_blocks[0]['record_blocks'])
     
     else:
         for key, values in clusters.items():
-            if key not in clusters_blocks.keys():
-                clusters_blocks[key] = []
-            
+            clusters_blocks.setdefault(key, set())
+
             for v in values:
-                b = v[:5]
-                if b not in clusters_blocks[key]:
-                    clusters_blocks[key].append(b)
+                record = list(filter(lambda record: record['record_ID'] == v[v.rfind('#') + 1:], dataset))
+                clusters_blocks[key].update(record[0]['record_blocks'])
 
 def calculate_signature(field):
-    field = field.lower()
-    field = field.replace(' ', '')
+    field = field.lower().replace(' ', '')
     
     signature = np.zeros(26, dtype=int)
     
@@ -76,14 +61,14 @@ def calculate_signature(field):
     
     return signature
 
-def can_skip_comparison(record1, record2):
+def can_skip_comparison(record1, record2, r1keys, r2keys):
 
     if len(record1) != len(record2): return False
-    if set(record1.keys()) != set(record2.keys()): return False
+    if set(r1keys) != set(r2keys): return False
 
     hamming_dist = []
 
-    for k in record1.keys():
+    for k in r1keys:
         sig1 = calculate_signature(record1[k])
         sig2 = calculate_signature(record2[k])
 
@@ -91,16 +76,9 @@ def can_skip_comparison(record1, record2):
 
     return all(dist > 2 * THETA for dist in hamming_dist)
 
-def edit_distance(record1, record2):
-    edit_distance = []
-    r2keys = list(record2.keys())
+def edit_distance(record1, record2, r1keys, r2keys):
 
-    for k1 in record1.keys():
-        for k2 in r2keys:
-            if k1 == k2:
-                edit_distance.append(lev.distance(record1[k1], record2[k2]))
-                r2keys.remove(k2)
-                break
+    edit_distance = [lev.distance(record1[k], record2[k]) for k in r1keys if k in r2keys]
     
     return all(dist <= THETA for dist in edit_distance)
 
@@ -111,8 +89,7 @@ def firla(dataset):
     concatenated_records = {f'{i + offset}': concatenate_attributes(dataset[i]) for i in range(len(dataset))}
     sorted_records = { k: v for k,v in sorted(concatenated_records.items(), key=lambda item: item[1])}
     deduplication(sorted_records)
-    
-    blocking()
+    blocking(dataset)
 
     for cluster in list(clusters.keys()):
         
@@ -120,31 +97,31 @@ def firla(dataset):
         
         for rep_record in clusters[cluster]:
             
-            with open(rep_record[rep_record.rfind('#') + 1:], 'r', encoding='utf-8') as rep_file:
-                rep_record_json = json.load(rep_file)
-                rep_record_json = decode_unicode_escapes(rep_record_json)
+            record_data = list(filter(lambda record: record['record_ID'] == rep_record[rep_record.rfind('#') + 1:], dataset))
+            r1keys = list(filter(lambda k: k != 'record_ID' and k != 'record_blocks', record_data[0].keys()))
             
-            block = rep_record[:5]
-            candidate_clusters = [k for k, v in clusters_blocks.items() if block in v]
-            
-            for candidate_cluster in candidate_clusters:
+            for block in record_data[0]['record_blocks']:
                 
-                if candidate_cluster == cluster:
-                    continue
+                candidate_clusters = [k for k, v in clusters_blocks.items() if block in v and k != cluster]
                 
-                for candidate_record in clusters[candidate_cluster]:
-                    with open(candidate_record[candidate_record.rfind('#') + 1:], 'r', encoding='utf-8') as candidate_file:
-                        candidate_record_json = json.load(candidate_file)
-                        candidate_record_json = decode_unicode_escapes(candidate_record_json)
-                    
-                    if can_skip_comparison(rep_record_json, candidate_record_json):
-                        is_clustered = True
-                        continue
-                    
-                    if  edit_distance(rep_record_json, candidate_record_json):
-                        clusters[cluster].extend(clusters[candidate_cluster])
-                        clusters[candidate_cluster] = []
-                        is_clustered = True
+                for candidate_cluster in candidate_clusters:
+
+                    for candidate_record in clusters[candidate_cluster]:
+                        
+                        candidate_record_data = list(filter(lambda record: record['record_ID'] == candidate_record[candidate_record.rfind('#') + 1:], dataset))
+                        r2keys = list(filter(lambda k: k != 'record_ID' and k != 'record_blocks', candidate_record_data[0].keys()))
+                        
+                        if can_skip_comparison(record_data[0], candidate_record_data[0], r1keys, r2keys):
+                            is_clustered = True
+                            continue
+                        
+                        if  edit_distance(record_data[0], candidate_record_data[0], r1keys, r2keys):
+                            clusters[cluster].extend(clusters[candidate_cluster])
+                            clusters[candidate_cluster] = []
+                            is_clustered = True
+                            break
+
+                    if is_clustered:
                         break
                 if is_clustered:
                     break
@@ -156,37 +133,44 @@ def firla(dataset):
 
 def read_dataset_sources(dataset_path):
     iter = 1
-    
-    for root, _, files in os.walk(dataset_path):
-        
-        cur_table = []
-        
-        for file in files:
-            if file.endswith(".json"):
-                
-                file_path = os.path.join(root, file)
-                
-                with open(file_path, 'r', encoding='utf-8') as json_file:
-                    data = json.load(json_file)
-                    data['record_ID'] = file_path
-                    decoded_data = decode_unicode_escapes(data)
-                    cur_table.append(decoded_data)
-        
-        if len(files) > 0:
-            print(f'\n--------------------- ITERAZIONE {iter} ---------------------\n')
-            result = firla(cur_table)
+    total_time = 0
 
-            with open(f'firla\\clusters\\iteration{iter}.json', 'w', encoding='utf-8') as res_file:
-                json.dump(result, res_file, indent=4, ensure_ascii=False)
-            
-            iter += 1
+    for subdir in os.listdir(dataset_path):
+        cur_table = list(filter(lambda record: subdir in record['record_ID'], schema_mapping))
+        
+        print(f'\n--------------------- ITERATION {iter} ---------------------')    
+        start_time = time.time()
+        
+        result = firla(cur_table)
+        
+        end_time = time.time()
+        execution_time = end_time - start_time
+        total_time += execution_time
+
+        print(f'Iteration time Time: {execution_time:.2f} s')
+        print(f'Execution Time: {total_time:.2f} s')
+
+        with open(f'firla\\clusters\\iteration{iter}.json', 'w', encoding='utf-8') as res_file:
+            json.dump(result, res_file, indent=4, ensure_ascii=False)
+        
+        iter += 1
+        break
 
 if __name__ == "__main__":
     offset = 0
     clusters = {}
     clusters_blocks = {}
 
-    with open('mediated_schema\\mediated_schema.json', 'r', encoding='utf-8') as json_file:
-        mediated_schema = json.load(json_file)
+    with open('firla\\schema_mapping_firla.json', 'r', encoding='utf-8') as schema_file:
+        schema_mapping = json.load(schema_file)
     
+    profiler = cProfile.Profile()
+    profiler.enable()
     read_dataset_sources(DATASET_PATH)
+    profiler.disable()
+    profiler.dump_stats("profile_output.prof")
+
+    # Load the profiling results
+    stats = pstats.Stats("profile_output.prof")
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats(100)
